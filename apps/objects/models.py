a2b5fc2c016b8ef72eb8f6a057874a7126f3e707
Dylachee <saaakbaevv@gmail.com>
2023-05-25 18:56:15 +0600

    Сделал корзину и заказ

--- apps/objects/models.py
+++ apps/objects/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -17,4 +20,23 @@ class TraditionalFood(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food = models.ForeignKey(TraditionalFood, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.pk}"
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food = models.ForeignKey(TraditionalFood, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
     
