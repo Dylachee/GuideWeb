53c3e03d53601eaf44bcd50de702642d4662c477
Dylachee <saaakbaevv@gmail.com>
2023-05-28 10:37:32 +0600

    add post method to clothes

--- apps/clothes/views.py
+++ apps/clothes/views.py
@@ -1,12 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics ,status
 from .models import TraditionalClothing
 from .serializers import TraditionalClothingSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-class TraditionalClothingList(generics.ListAPIView):
+class TraditionalClothingList(generics.ListCreateAPIView):
     queryset = TraditionalClothing.objects.all()
     serializer_class = TraditionalClothingSerializer
     permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = TraditionalClothingSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TraditionalClothingRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TraditionalClothing.objects.all()
